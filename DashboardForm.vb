e147371d8235b97a84dc1f872ae58d137d87ba0f
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2026-04-07 12:35:21 +0530

    Allow free dashboard resizing

--- DashboardForm.vb
+++ DashboardForm.vb
@@ -13,17 +13,47 @@ Public Class MainForm
     Public Sub New()
         Text = "Caspar Layer Monitor"
         StartPosition = FormStartPosition.CenterScreen
-        FormBorderStyle = FormBorderStyle.FixedDialog
-        MaximizeBox = False
+        FormBorderStyle = FormBorderStyle.Sizable
+        MaximizeBox = True
+        MinimumSize = New Size(220, 120)
         ClientSize = New Size(860, 470)
-
-        studioAControl.Location = New Point(10, 10)
-        studioAControl.Size = New Size(420, 430)
-
-        studioBControl.Location = New Point(430, 10)
-        studioBControl.Size = New Size(420, 430)
 
         Controls.Add(studioAControl)
         Controls.Add(studioBControl)
+
+        AddHandler Resize, AddressOf MainForm_Resize
+        LayoutStudioControls()
+    End Sub
+
+    Private Sub MainForm_Resize(sender As Object, e As EventArgs)
+        LayoutStudioControls()
+    End Sub
+
+    Private Sub LayoutStudioControls()
+        Const margin As Integer = 10
+        Const spacing As Integer = 10
+        Const singlePanelThreshold As Integer = 760
+
+        Dim availableHeight = Math.Max(80, ClientSize.Height - (margin * 2))
+        Dim availableWidth = Math.Max(80, ClientSize.Width - (margin * 2))
+
+        If ClientSize.Width < singlePanelThreshold Then
+            studioAControl.Location = New Point(margin, margin)
+            studioAControl.Size = New Size(availableWidth, availableHeight)
+            studioAControl.Visible = True
+
+            studioBControl.Visible = False
+            Return
+        End If
+
+        Dim dualWidth = Math.Max(80, (ClientSize.Width - (margin * 2) - spacing) \ 2)
+
+        studioAControl.Location = New Point(margin, margin)
+        studioAControl.Size = New Size(dualWidth, availableHeight)
+        studioAControl.Visible = True
+
+        studioBControl.Location = New Point(studioAControl.Right + spacing, margin)
+        studioBControl.Size = New Size(dualWidth, availableHeight)
+        studioBControl.Visible = True
     End Sub
 End Class
